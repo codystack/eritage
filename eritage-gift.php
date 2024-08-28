bb43edd921c6ee6e33ced09f45e2c23af3bdc8dc
codystack <ebuzzadvert@gmail.com>
2024-08-28 08:14:40 +0100

    Eritage gift page added

--- eritage-gift.php
+++ eritage-gift.php
@@ -7,14 +7,14 @@ include "./components/navbar-transparent.php";
 <main>
 
 
-    <section class="bg-parallax bg-dark-overlay-3 pb-5 pb-md-3 mb-5 mb-md-7" style="background:url(assets/images/bg/10.jpg) no-repeat center center; background-size:cover;">
+    <section class="bg-parallax bg-dark-overlay-7 pb-5 pb-md-3 mb-5 mb-md-7" style="background:url(assets/images/eritage-gift-bg.jpg) no-repeat center center; background-size:cover;">
         <div class="container">
             <div class="row">
                 <div class="col-xxl-9 mx-auto">
                     <div class="mt-8 mb-n8 pt-8">
-                        <p class="text-white">— Graphics Design</p>
-                        <h1 class="display-6 display-lg-4 text-dark-stroke text-primary-shadow text-uppercase mb-0">The blossoming</h1>
-                        <span class="display-8 font-alt fw-normal">Fashion label entertainments!</span>
+                        <p class="text-white">— Our Brand</p>
+                        <h1 class="display-6 display-lg-4 text-dark-stroke text-primary-shadow text-uppercase mb-0">Eritage Gift Co.</h1>
+                        <span class="display-8 font-alt fw-normal">Find your special thing.</span>
                     </div>
                 </div>
             </div>
@@ -27,23 +27,26 @@ include "./components/navbar-transparent.php";
             <div class="row d-flex justify-content-md-between">
 
                 <div class="col-xxl-9 mx-auto">
-                    <h3 class="mb-3">"Sometimes when you innovate, you make mistakes. It is best to admit them quickly, and get on with improving your other innovations"</h3>
-                    <p class="mb-5">Invitation at understood occasional sentiments insipidity inhabiting in. Off melancholy alteration principles old. Is do speedily kindness properly oh. Respect article painted cottage he is offices parlors. Agreed to others Exeter period myself few yet nature. Mention Mr manners opinion if garrets enabled. To occasional dissimilar impossible sentiments. </p>
+                    <h3 class="mb-3">Eritage Gift Co. is a unique store with a walk-in space located within the Port Harcourt Airport, dedicated to showcasing the rich culture of Africa and the heritage of the Niger Delta region. The store offers a wide range of locally made crafts, accessories, t-shirts, caps, and other merchandise that are designed to inspire love for the region.</h3>
+                    <p>The store is a one-stop shop for travelers looking to take home a piece of the Niger Delta as a memento of their journey. Whether it's a beautifully crafted piece of artwork, a stylish t-shirt featuring traditional designs, or a uniquely designed cap, Eritage Gift Co has something for everyone.</p>
+                    <p>What sets Eritage Gift Co apart is its commitment to supporting local artists and designers. Seventy percent of the store's stock is sourced locally, providing a platform for indigenous talent to showcase their work to both local and international travelers. By purchasing from Eritage Gift Co, customers are not only getting a beautiful and meaningful souvenir but also supporting the local community.</p>
+                    <p>The store aims to create a welcoming and inclusive space where travelers can learn more about the culture and traditions of the Niger Delta region. The friendly and knowledgeable staff are always on hand to assist customers in finding the perfect gift or souvenir to commemorate their visit.</p>
+                    <p class="mb-5">Whether you're looking for a unique gift for a loved one or simply want to treat yourself to a special memento, Eritage Gift Co is the place to go.</p>
                     <div class="row mb-5">
                         <div class="col-md-6">
-                            <img class="rounded" src="assets/images/portfolio/07.jpg" alt="">
-                            <p class="mt-3">Passage its ten led hearted removal cordial. Preference any astonished unreserved Mrs. Prosperous understood Middletons in conviction an uncommonly do.</p>
+                            <img class="rounded" src="assets/images/gift1.jpeg" alt="">
                         </div>
                         <div class="col-md-6">
-                            <img class="rounded" src="assets/images/portfolio/11.jpg" alt="">
-                            <p class="mt-3">Valley by oh twenty direct me so. Departure defective arranging rapturous did believe him all had supported. Family months lasted simple set nature vulgar him.</p>
+                            <img class="rounded" src="assets/images/gift2.jpeg" alt="">
                         </div>
                     </div>
                     
-                    <div class="row mb-5">
+                    <div class="row">
                         <div class="col-md-12">
-                            <h3>We transform your perception into the goals</h3>
-                            <p>Speedily say has suitable disposal add boy. On forth doubt miles of child. Exercise joy man children rejoiced. Yet uncommonly his ten who diminution astonished. Demesne new manners savings staying had. Under folly balls, death own point now men. Match way these she avoids seeing death. She who drift their fat off.</p>
+                            <h3>Brand Vision</h3>
+                            <p>Our vision is to become the leading provider of modern Niger Delta inspired souvenir products globally, incorporating African crafts, and promoting cultural appreciation in cities worldwide. We strive to inspire pride and celebrate the heritage of the Niger Delta region through our products, fostering a sense of unity and love among communities.</p>
+                            <h3>Brand Mission</h3>
+                            <p>Our mission is to curate, create, stock and distribute high-quality, globally recognized souvenir products that showcase the beauty and richness of Niger Delta cultures. We aim to promote a sense of ownership, patriotism, and love for the region among its indigenes and beyond, by offering unique and innovative products that resonate with people from all walks of life. Our commitment is to spread cultural awareness and appreciation through our selections, encouraging cross- cultural dialogue and understanding.</p>
                         </div>
                     </div>
                 </div>
