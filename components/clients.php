2bd367e97282d9ecf648d22323ff2c92afc075c6
codystack <ebuzzadvert@gmail.com>
2024-08-28 22:32:17 +0100

    clients logo added

--- components/clients.php
+++ components/clients.php
@@ -1,79 +1,76 @@
     <section class="bg-dark position-relative rounded pattern-overlay-3 my-4 mx-xl-3 mx-xxxl-9">
         <div class="container">
-            <!-- Title -->
             <div class="row mb-5">
                 <div class="col-md-12 ">
                     <h2 class="display-6 text-white-stroke text-center">Brand we've worked with:</h2>
                 </div>
             </div>
-            <!-- Client logo -->
             <div class="row d-flex justify-content-center">
-                <!-- Logo item -->
                 <div class="col-6 col-sm-4 col-lg-2"> 
                     <div class="mb-4 p-4 grayscale text-center opacity-4">
-                        <img src="assets/images/clients/light/01.svg" alt="">
+                        <img src="assets/images/clients/logo-1.png" alt="">
                     </div>
                 </div>
-                <!-- Logo item -->
                 <div class="col-6 col-sm-4 col-lg-2"> 
                     <div class="mb-4 p-4 grayscale text-center opacity-4">
-                        <img src="assets/images/clients/light/02.svg" alt="">
+                        <img src="assets/images/clients/logo-2.png" alt="">
                     </div>
                 </div>
-                <!-- Logo item -->
                 <div class="col-6 col-sm-4 col-lg-2"> 
                     <div class="mb-4 p-4 grayscale text-center opacity-4">
-                        <img src="assets/images/clients/light/03.svg" alt="">
+                        <img src="assets/images/clients/logo-3.png" alt="">
                     </div>
                 </div>
-                <!-- Logo item -->
                 <div class="col-6 col-sm-4 col-lg-2"> 
                     <div class="mb-4 p-4 grayscale text-center opacity-4">
-                        <img src="assets/images/clients/light/04.svg" alt="">
+                        <img src="assets/images/clients/logo-4.png" alt="">
                     </div>
                 </div>
-                <!-- Logo item -->
                 <div class="col-6 col-sm-4 col-lg-2"> 
                     <div class="mb-4 p-4 grayscale text-center opacity-4">
-                        <img src="assets/images/clients/light/05.svg" alt="">
+                        <img src="assets/images/clients/logo-5.png" alt="">
                     </div>
                 </div>
-                <!-- Logo item -->
                 <div class="col-6 col-sm-4 col-lg-2"> 
                     <div class="mb-4 p-4 grayscale text-center opacity-4">
-                        <img src="assets/images/clients/light/06.svg" alt="">
+                        <img src="assets/images/clients/logo-6.png" alt="">
                     </div>
                 </div>
-                <!-- Logo item -->
                 <div class="col-6 col-sm-4 col-lg-2"> 
                     <div class="mb-4 p-4 grayscale text-center opacity-4">
-                        <img src="assets/images/clients/light/07.svg" alt="">
+                        <img src="assets/images/clients/logo-7.png" alt="">
                     </div>
                 </div>
-                <!-- Logo item -->
                 <div class="col-6 col-sm-4 col-lg-2"> 
                     <div class="mb-4 p-4 grayscale text-center opacity-4">
-                        <img src="assets/images/clients/light/08.svg" alt="">
+                        <img src="assets/images/clients/logo-8.png" alt="">
                     </div>
                 </div>
-                <!-- Logo item -->
                 <div class="col-6 col-sm-4 col-lg-2"> 
                     <div class="mb-4 p-4 grayscale text-center opacity-4">
-                        <img src="assets/images/clients/light/09.svg" alt="">
+                        <img src="assets/images/clients/logo-9.png" alt="">
                     </div>
                 </div>
-                <!-- Logo item -->
                 <div class="col-6 col-sm-4 col-lg-2"> 
                     <div class="mb-4 p-4 grayscale text-center opacity-4">
-                        <img src="assets/images/clients/light/10.svg" alt="">
+                        <img src="assets/images/clients/logo-10.png" alt="">
                     </div>
                 </div>
-                <!-- Logo item -->
                 <div class="col-6 col-sm-4 col-lg-2"> 
                     <div class="mb-4 p-4 grayscale text-center opacity-4">
-                        <img src="assets/images/clients/light/11.svg" alt="">
+                        <img src="assets/images/clients/logo-11.png" alt="">
                     </div>
                 </div>
-            </div><!-- row END -->
+                <div class="col-6 col-sm-4 col-lg-2"> 
+                    <div class="mb-4 p-4 grayscale text-center opacity-4">
+                        <img src="assets/images/clients/logo-12.png" alt="">
+                    </div>
+                </div>
+                <div class="col-6 col-sm-4 col-lg-2"> 
+                    <div class="mb-4 p-4 grayscale text-center opacity-4">
+                        <img src="assets/images/clients/logo-13.png" alt="">
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
