e991a842279dd099c20f8245ec67d3c6b23e1860
codystack <ebuzzadvert@gmail.com>
2024-09-01 15:48:35 +0100

    Services modified

---
+++ brand-strategy.php
@@ -0,0 +1,72 @@
+<?php
+$page = "Brand Strategy";
+include "./components/header.php";
+include "./components/navbar.php";
+?>
+
+<main>
+
+    <section class=" text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-lg-8 mx-auto">
+                    <h1 class="display-5 text-primary">Brand Strategy</h1>
+                    <p>Eritage Africa specializes in brand strategy services, providing clients with comprehensive solutions to establish, elevate, and sustain their brand presence. Our offerings are designed to harness our wealth of experience, diverse team of professionals, creative acumen, and commitment to fostering the growth and sustainability of indigenous brands.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="pt-5 pb-0">
+        <div class="container">
+            <div class="row mb-4" data-sticky-container>
+                <div class="col-md-6">
+                    <a class="mb-4 d-block" data-glightbox data-gallery="portfolio-req" href="assets/images/brandstrategy.jpg" >
+                        <img class="rounded" src="assets/images/brandstrategy.jpg" alt="">
+                    </a>
+                </div>
+                <div class="col-md-6">
+                    <div data-sticky data-margin-top="100" data-margin-bottom="100" data-sticky-for="767">
+                        <p>Here are a few things we can do for you:</p>
+                        <ul class="list-group list-group-borderless list-group-icon-primary-bg mb-2">
+                            <li class="list-group-item"><i class="fas fa-check"></i><strong>Brand Discovery and Analysis:</strong> -	Market research and trend analysis.    – Brand audits to evaluate current brand positioning and effectiveness.    – Competitive landscape assessment.</li>
+                            <li class="list-group-item"><i class="fas fa-check"></i><strong>Brand Positioning and Messaging:</strong>    - Development of unique value propositions.    – Crafting compelling brand messaging.    – Establishment of brand positioning strategies.</li>
+                            <li class="list-group-item"><i class="fas fa-check"></i><strong>Brand Identity and Design:</strong>    - Creation of visually appealing brand identities, including logos and brand collateral.    – Brand style guidelines for consistency across all communication channels.    – Packaging design and product branding.</li>
+                            <li class="list-group-item"><i class="fas fa-check"></i><strong>Brand Communication Strategy:</strong>    - Creation of integrated communication plans.    – Content development for traditional and digital platforms.    – Social media strategy and management.</li>
+                            <li class="list-group-item"><i class="fas fa-check"></i><strong>Brand Activation and Engagement:</strong>    - Development and execution of brand activation campaigns.    – Event planning and execution for brand engagement.    – Influencer marketing strategies.</li>
+                            <li class="list-group-item"><i class="fas fa-check"></i><strong>Brand Sustainability and Growth:</strong>    - Long-term brand strategy development.    – Expansion planning and brand portfolio management.    – Support and guidance for brand evolution and adaptation.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-6 mb-4" data-sticky-container>
+                <div class="col-md-6 order-last order-md-1">
+                    <div data-sticky data-margin-top="100" data-margin-bottom="100" data-sticky-for="767">
+                        <p>Clients benefit from Eritage Africa’s services through:</p>
+                        <ul class="list-group list-group-borderless list-group-icon-primary-bg">
+                            <li class="list-group-item"><i class="fas fa-check"></i><strong>Experience:</strong> Draw on our extensive experience in brand strategy and marketing.</li>
+                            <li class="list-group-item"><i class="fas fa-check"></i><strong>Team of Professionals:</strong> Collaborate with a diverse team of skilled professionals.</li>
+                            <li class="list-group-item"><i class="fas fa-check"></i><strong>Creativity:</strong> Unlock innovative and creative solutions for brand growth.</li>
+                            <li class="list-group-item"><i class="fas fa-check"></i><strong>Indigenous Brand Focus:</strong> Receive tailored support that honors cultural heritage.</li>
+                            <li class="list-group-item"><i class="fas fa-check"></i><strong>Sustainability:</strong> Partner with us for long-term brand sustainability and success.</li>
+                        </ul>
+                        <p>Through these tailored services and benefits, clients can expect a strategic partner dedicated to realizing their brand’s full potential in the market.</p>
+                        <h6><a href="https://posts.gle/qn1jdT" target="_blank" class="text-primary">Click here</a> to see a sample file</h6>
+                    </div>
+                </div>
+                <div class="col-md-6 order-1 order-md-last">
+                    <a class="mb-4 d-block" data-glightbox data-gallery="portfolio-req" href="assets/images/brandstrategy1.jpg">
+                        <img class="rounded" src="assets/images/brandstrategy1.jpg" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+
+    <?php include "./components/cta.php"; ?>
+
+</main>
+
+<?php include "./components/footer.php"; ?>
